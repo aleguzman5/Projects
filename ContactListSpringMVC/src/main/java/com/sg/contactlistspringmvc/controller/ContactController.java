3dcedd5d92ee0754f0f1773409e0fc844d13f20e
aleguzman <shmc555@gmail.com>
2017-10-05 12:58:43 -0500

    New Contact, Display Contact Details, Delete Contact, Form Validation (Contact List Spring MVC)

--- ContactListSpringMVC/src/main/java/com/sg/contactlistspringmvc/controller/ContactController.java
+++ ContactListSpringMVC/src/main/java/com/sg/contactlistspringmvc/controller/ContactController.java
@@ -5,7 +5,14 @@
  */
 package com.sg.contactlistspringmvc.controller;
 
+import com.sg.contactlistspringmvc.dao.ContactListDao;
+import com.sg.contactlistspringmvc.model.Contact;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,9 +22,78 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class ContactController {
-    
-    @RequestMapping(value="/displayContactsPage", method = RequestMethod.GET)
-    public String displayContactPage() {
+
+    ContactListDao dao;
+
+    @Inject
+    public ContactController(ContactListDao dao) {
+        this.dao = dao;
+    }
+
+    @RequestMapping(value = "/displayContactsPage", method = RequestMethod.GET)
+    public String displayContactPage(Model model) {
+
+        List<Contact> contactList = dao.getAllContacts();
+
+        model.addAttribute("contactList", contactList);
+
         return "contacts";
+    }
+
+    @RequestMapping(value = "/createContact", method = RequestMethod.POST)
+    public String createContact(HttpServletRequest request) {
+        // grab the incoming values from the form and create a new Contact
+        // object
+        Contact contact = new Contact();
+        contact.setFirstName(request.getParameter("firstName"));
+        contact.setLastName(request.getParameter("lastName"));
+        contact.setCompany(request.getParameter("company"));
+        contact.setPhone(request.getParameter("phone"));
+        contact.setEmail(request.getParameter("email"));
+
+        // persist the new Contact
+        dao.addContact(contact);
+
+        // we don't want to forward to a View component - we want to
+        // redirect to the endpoint that displays the Contacts Page
+        // so it can display the new Contact in the table.
+        return "redirect:displayContactsPage";
+    }
+
+    @RequestMapping(value = "/displayContactDetails", method = RequestMethod.GET)
+    public String displayContactDetails(HttpServletRequest request, Model model) {
+        String contactIdParameter = request.getParameter("contactId");
+        int contactId = Integer.parseInt(contactIdParameter);
+
+        Contact contact = dao.getContactById(contactId);
+
+        model.addAttribute("contact", contact);
+
+        return "contactDetails";
+    }
+
+    @RequestMapping(value = "/deleteContact", method = RequestMethod.GET)
+    public String deleteContact(HttpServletRequest request) {
+        String contactIdParameter = request.getParameter("contactId");
+        long contactId = Long.parseLong(contactIdParameter);
+        dao.removeContact(contactId);
+        return "redirect:displayContactsPage";
+    }
+
+    @RequestMapping(value = "/displayEditContactForm", method = RequestMethod.GET)
+    public String displayEditContactForm(HttpServletRequest request, Model model) {
+        String contactIdParameter = request.getParameter("contactId");
+        long contactId = Long.parseLong(contactIdParameter);
+        Contact contact = dao.getContactById(contactId);
+        model.addAttribute("contact", contact);
+        return "editContactForm";
+    }
+
+    @RequestMapping(value = "/editContact", method = RequestMethod.POST)
+    public String editContact(@ModelAttribute("contact") Contact contact) {
+
+        dao.updateContact(contact);
+
+        return "redirect:displayContactsPage";
     }
 }
