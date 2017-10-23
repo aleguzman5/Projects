df8ea5047fbf547d8d15d0c3bf0673459105fdda
aleguzman <shmc555@gmail.com>
2017-10-23 13:33:46 -0500

    SuperHero Sightings(Test in LocationDao)

--- SuperheroSightings/src/test/java/com/sg/superherosightings/dao/LocationDaoTest.java
+++ SuperheroSightings/src/test/java/com/sg/superherosightings/dao/LocationDaoTest.java
@@ -6,6 +6,13 @@
 package com.sg.superherosightings.dao;
 
 import com.sg.superherosightings.model.Location;
+import com.sg.superherosightings.model.Organization;
+import com.sg.superherosightings.model.Sighting;
+import com.sg.superherosightings.model.Super;
+import com.sg.superherosightings.model.SuperPower;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -24,6 +31,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class LocationDaoTest {
 
     LocationDao locationDao;
+    OrganizationDao organizationDao;
+    SuperDao superDao;
+    SightingDao sightingDao;
+    SuperPowerDao superPowerDao;
 
     public LocationDaoTest() {
     }
@@ -40,9 +51,28 @@ public class LocationDaoTest {
     public void setUp() {
         ApplicationContext ctx
                 = new ClassPathXmlApplicationContext("test-applicationContext.xml");
-
+        sightingDao = ctx.getBean("sightingDao", SightingDao.class);
+        superDao = ctx.getBean("superDao", SuperDao.class);
+        superPowerDao = ctx.getBean("superPowerDao", SuperPowerDao.class);
+        organizationDao = ctx.getBean("organizationDao", OrganizationDao.class);
         locationDao = ctx.getBean("locationDao", LocationDao.class);
 
+        List<Sighting> sightings = sightingDao.getAllSightings();
+        for (Sighting currentSighting : sightings) {
+            sightingDao.deleteSighting(currentSighting.getSightingId());
+        }
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+        for (Organization currentOrganization : organizations) {
+            organizationDao.deleteOrganization(currentOrganization.getOrganizationId());
+        }
+        List<Super> supers = superDao.getAllSupers();
+        for (Super currentSuper : supers) {
+            superDao.deleteSuper(currentSuper.getSuperId());
+        }
+        List<SuperPower> superPowers = superPowerDao.getAllSuperPowers();
+        for (SuperPower currentSP : superPowers) {
+            superPowerDao.deleteSuperPower(currentSP.getSuperPowerId());
+        }
         List<Location> locations = locationDao.getAllLocations();
         for (Location currentLocation : locations) {
             locationDao.deleteLocation(currentLocation.getLocationId());
@@ -105,7 +135,7 @@ public class LocationDaoTest {
         loc.setLongitude(-98.432156);
 
         locationDao.addLocation(loc);
-        
+
         Location fromDao = locationDao.getLocationById(loc.getLocationId());
         assertEquals(fromDao.getName(), "The Software Guild");
 
@@ -125,7 +155,7 @@ public class LocationDaoTest {
      */
     @Test
     public void testGetAllLocations() {
-        
+
         Location loc = new Location();
         loc.setName("Metropolis");
         loc.setDescription("City of Superman");
@@ -151,6 +181,65 @@ public class LocationDaoTest {
      */
     @Test
     public void testGetAllLocationsBySuperId() {
+        SuperPower sp = new SuperPower();
+        sp.setName("Fly");
+
+        superPowerDao.addSuperPower(sp);
+
+        Organization org = new Organization();
+        org.setName("Avengers");
+        org.setDescription("The best");
+        org.setAddress("123 Imaginary World");
+        org.setCity("No Land");
+        org.setZip(11111);
+        org.setPhone(222222222);
+
+        organizationDao.addOrganization(org);
+
+        Super s = new Super();
+        s.setName("Batman");
+        s.setDescription("Even better");
+        s.setSuperPower(sp);
+        List<Organization> organizations = new ArrayList<>();
+        organizations.add(org);
+        s.setOrganizations(organizations);
+
+        superDao.addSuper(s);
+
+        Location loc = new Location();
+        loc.setName("The Software Guild");
+        loc.setDescription("Coding Camp");
+        loc.setAddress("5432 Main Ave.");
+        loc.setLatitude(44.032145);
+        loc.setLongitude(-98.432156);
+        locationDao.addLocation(loc);
+
+        Location loc2 = new Location();
+        loc2.setName("Gotham");
+        loc2.setDescription("Batman's City");
+        loc2.setAddress("9999 Gotham St.");
+        loc2.setLatitude(87.9847484);
+        loc2.setLongitude(-23.9898989);
+        locationDao.addLocation(loc2);
+
+        Sighting si = new Sighting();//date-loc-super
+        si.setDate(LocalDate.parse("2017-10-19",
+                DateTimeFormatter.ISO_DATE));
+        si.setLocation(loc);
+        List<Super> supers = new ArrayList<>();
+        supers.add(s);
+        si.setSupers(supers);
+        
+        Sighting si2 = new Sighting();//date-loc-super
+        si2.setDate(LocalDate.parse("2017-10-23",
+                DateTimeFormatter.ISO_DATE));
+        si2.setLocation(loc2);
+        si2.setSupers(supers);
+        
+        sightingDao.addSighting(si);
+        sightingDao.addSighting(si2);
+        
+        assertEquals(2, locationDao.getAllLocationsBySuperId(s.getSuperId()).size());
     }
 
 }
